afc4da54ba4d927d1d6591e171a388d914da56b8
kobeline2 <kobeline2@gmail.com>
2024-01-21 13:09:31 +0900

    prediction function added

--- main.m
+++ main.m
@@ -2,26 +2,20 @@
 clear; clc;
 run('parameters.m');
 Data = load(Param.dataPath);
+Data = preprocessData(Data, Param);
+Data = makeTrainTestData(Data, Param);
+[Data, ParamNorm] = normalizeData(Data, Param);
 if ~Param.isprediction
-    Data = preprocessData(Data, Param);
-    Data = makeTrainTestData(Data, Param);
-    [Data, ParamNorm] = normalizeData(Data, Param);
     h  = feval(Param.modelval, Data, Param);
+    ypred = feval(Param.modelfit, Data, Param, h, Data.Xtest);
+else
+    DataPred = load(Param.predictData);
+    DataPred.X = DataPred.C{:, Param.Xindex};
+    DataPred = rmfield(DataPred, "C");
+    load(Param.hyp);
+    load(Param.ParamNorm);
+    [DataPred, ParamNorm] = normalizeData(DataPred, Param, ParamNorm);
+    ypred = feval(Param.modelfit, Data, Param, h, DataPred.X);
 end
-ypred = feval(Param.modelfit, Data, Param, h, Data.Xtest);
 evaluateModel(ypred(:, 1), Data.ytest);
 postprocessResults(Data, ypred, ParamNorm, Param)
-
-% % 結果の保存
-% save('results/results.mat', 'results');
-% % 後処理と結果の可視化
-% postProcessedResults = postprocessResults(results);
-% save('results/analysis/postProcessedResults.mat', 'postProcessedResults');
-% 
-% % グラフの生成と保存
-% fig = createFigures(postProcessedResults);
-% saveas(fig, 'results/figures/resultFigures.fig');
-% 
-% % 環境のクリーンアップ
-% rmpath('src');
-% rmpath('src/utils');
