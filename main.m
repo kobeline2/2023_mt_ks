035f3d6a221047c63e95bf84a51acc9119d11de1
kobeline2 <kobeline2@gmail.com>
2024-01-21 15:59:21 +0900

    prediction function added

--- main.m
+++ main.m
@@ -1,21 +1,23 @@
 % main.m - main code of the project
 clear; clc;
 run('parameters.m');
-Data = load(Param.dataPath);
+Data = load(Param.Data);
 Data = preprocessData(Data, Param);
 Data = makeTrainTestData(Data, Param);
 [Data, ParamNorm] = normalizeData(Data, Param);
 if ~Param.isprediction
     h  = feval(Param.modelval, Data, Param);
     ypred = feval(Param.modelfit, Data, Param, h, Data.Xtest);
+    evaluateModel(ypred(:, 1), Data.ytest);
 else
     DataPred = load(Param.predictData);
-    DataPred.X = DataPred.C{:, Param.Xindex};
-    DataPred = rmfield(DataPred, "C");
     load(Param.hyp);
     load(Param.ParamNorm);
+    DataPred.X = DataPred.C{:, Param.Xindex};
     [DataPred, ParamNorm] = normalizeData(DataPred, Param, ParamNorm);
     ypred = feval(Param.modelfit, Data, Param, h, DataPred.X);
 end
-evaluateModel(ypred(:, 1), Data.ytest);
-postprocessResults(Data, ypred, ParamNorm, Param)
+Res = postprocessResults(Data, ypred, ParamNorm, Param);
+if Param.outputExcel
+    outputPredictionExcel(Res, Param);
+end
--- src/postprocessing/postprocessResults.m
+++ src/postprocessing/postprocessResults.m
@@ -1,37 +1,57 @@
-function postprocessResults(Data, ymean, ParamNorm, Param)
-figure;
+function Res = postprocessResults(Data, ymean, ParamNorm, Param)
+
 if size(ymean, 2) == 2
-    nsigma = 2;
+    % nsigma = 2; % currently output \pm 1, 2 \sigma
     ystd  = sqrt(ymean(:, 2));
     ymean = ymean(:, 1);
-    yupper  = (ymean + nsigma*ystd) * ParamNorm.sy + ParamNorm.cy;
-    ylower  = (ymean - nsigma*ystd) * ParamNorm.sy + ParamNorm.cy;
+    yplus1simga   = (ymean + 1*ystd) * ParamNorm.sy + ParamNorm.cy;
+    yplus2sigma   = (ymean + 2*ystd) * ParamNorm.sy + ParamNorm.cy;
+    yminus1sigma  = (ymean - 1*ystd) * ParamNorm.sy + ParamNorm.cy;
+    yminus2sigma  = (ymean - 2*ystd) * ParamNorm.sy + ParamNorm.cy;
     ymean   = ymean * ParamNorm.sy + ParamNorm.cy;
-    yactual = Data.ytest * ParamNorm.sy + ParamNorm.cy;
+    if ~Param.isprediction
+        yactual = Data.ytest * ParamNorm.sy + ParamNorm.cy;
+    end
+
     if Param.logy
-        yupper  = exp(yupper)*Param.coefLog;
-        ylower  = exp(ylower)*Param.coefLog;
-        ymean   = exp(ymean)*Param.coefLog;
-        yactual = exp(yactual)*Param.coefLog;
+        yplus1simga  = exp(yplus1simga)*Param.coefLog;
+        yplus2sigma  = exp(yplus2sigma)*Param.coefLog;
+        yminus1sigma = exp(yminus1sigma)*Param.coefLog;
+        yminus2sigma = exp(yminus2sigma)*Param.coefLog;
+        ymean        = exp(ymean)*Param.coefLog;
+        if ~Param.isprediction
+            yactual = exp(yactual)*Param.coefLog;
+        end
+    end
+    if ~Param.isprediction
+        figure;
+        z = 1:length(ymean);
+        f = [yplus2sigma; flip(yminus2sigma,1)]; 
+        fill([z'; flip(z, 2)'], f,...
+            'k', ...
+            LineStyle = "none",...
+            FaceAlpha = 0.3);
+        hold on; 
+        plot(z, ymean, 'k', LineWidth=1)
+        plot(z, yactual, 'r', LineWidth=1)
+        legend('{\pm}2\sigma', 'mean', 'actual')
     end
     
-    z = 1:length(ymean);
-    f = [yupper; flip(ylower,1)]; 
-    f = fill([z'; flip(z, 2)'], f,...
-        'k', ...
-        LineStyle = "none",...
-        FaceAlpha = 0.3);
-    hold on; 
-    plot(z, ymean, 'k', LineWidth=1)
-    plot(z, yactual, 'r', LineWidth=1)
-    legend('{\pm}2\sigma', 'mean', 'actual')
-
 elseif size(ymean, 2) == 1
     % plot(mexp_pred, 'k')
     % hold on
     % plot(mexp_psigma, 'r')
     % plot(mexp_msigma, 'r')
     % plot(C.Qs, 'b', 'LineWidth',2)
+end
+
+Res.yplus1simga  = yplus1simga ; 
+Res.yplus2sigma  = yplus2sigma ;
+Res.yminus1sigma = yminus1sigma;
+Res.yminus2sigma = yminus2sigma;
+Res.ymean        = ymean       ; 
+if ~Param.isprediction
+    Res.yactual = yactual;
 end
 
 end
