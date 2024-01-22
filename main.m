7ac117a33ae4f9d8ff11e890bbf1f267b7ac7056
kobeline2 <kobeline2@gmail.com>
2024-01-22 17:28:57 +0900

    hot fix

--- main.m
+++ main.m
@@ -1,21 +1,21 @@
 % main.m - main code of the project
 clear; clc;
 run('parameters.m');
-Data = load(Param.Data);
-Data = preprocessData(Data, Param);
-Data = makeTrainTestData(Data, Param);
-[Data, ParamNorm] = normalizeData(Data, Param);
-if ~Param.isprediction
-    h  = feval(Param.modelval, Data, Param);
-    ypred = feval(Param.modelfit, Data, Param, h, Data.Xtest);
-    evaluateModel(ypred(:, 1), Data.ytest);
-else
+load(Param.Data);
+if Param.isprediction % prediction
     DataPred = load(Param.predictData);
     load(Param.hyp);
     load(Param.ParamNorm);
     DataPred.X = DataPred.C{:, Param.Xindex};
     [DataPred, ParamNorm] = normalizeData(DataPred, Param, ParamNorm);
     ypred = feval(Param.modelfit, Data, Param, h, DataPred.X);
+else % Learning
+    Data = preprocessData(Data, Param);
+    Data = makeTrainTestData(Data, Param);
+    [Data, ParamNorm] = normalizeData(Data, Param); 
+    h  = feval(Param.modelval, Data, Param);
+    ypred = feval(Param.modelfit, Data, Param, h, Data.Xtest);
+    evaluateModel(ypred(:, 1), Data.ytest);
 end
 Res = postprocessResults(Data, ypred, ParamNorm, Param);
 if Param.outputExcel
---
+++ snipet.m
@@ -0,0 +1,15 @@
+% for run
+run('init.m')
+run('main.m')
+
+%% for save params
+name = '240122sato';
+m = ['mkdir ', sprintf('res/param/%s/', name)];
+eval(m)
+save(sprintf('res/param/%s/h', name), 'h')
+save(sprintf('res/param/%s/Data', name), 'Data')
+save(sprintf('res/param/%s/ParamNorm', name), 'ParamNorm')
+m = ['!cp parameters.m ', sprintf('res/param/%s/', name)];
+eval(m)
+
+%% 
